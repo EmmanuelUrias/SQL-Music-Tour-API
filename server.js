4ac15c749263da0b1ea36c4c88f6373259199b2f
EmmanuelU <emmanuelurias60@icloud.com>
2023-02-01 19:11:11 -0800

    implemented sequalize

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'eu1208007!'
+  })
+
+  try {
+    sequelize.authenticate()
+    console.log(`connected with Sequalize`)
+  } catch(err) {
+    console.log(`unable to connect to PG: ${err}`)
+  }
+  
 
 // ROOT
 app.get('/', (req, res) => {
